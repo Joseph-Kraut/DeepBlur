2ab039140cb7eeab57500aa1b3d0575ea7c1ae45
Marshall Fisher <mfisher@berkeley.edu>
2019-04-07 19:41:56 -0700

    Minor comments / remove old preprocess_images.py

--- sample_tester.py
+++ sample_tester.py
@@ -1,5 +1,6 @@
 from preprocess_images import get_samples
 
-filename = 'firefox.png'
+filename = 'nature.jpg'
 
-get_samples(filename, 10, 5, should_blur=False)
+get_samples(filename, 50, 50, should_blur=True, output_dir='blurry')
+get_samples(filename, 50, 50, should_blur=False, output_dir='truthy')
--- src/pipeline.py
+++ src/pipeline.py
@@ -9,7 +9,7 @@ def build_batch(blur_dir, truth_dir, batch_size):
     Generates a mini-batch of numpy arrays; uses a generator to truly create epochs.
     IMPORTANT: Assumes truth_dir contains exactly the same set of file names as truth_dir, where the
     files are the corresponding truth image samples. 
-    
+
     Returns:
         data_batch = List[np.array], of size batch_size, containing blurred image pixel arrays.
         truth_batch = List[np.array], of size batch_size, containing the corresponding ground truths.
@@ -20,9 +20,9 @@ def build_batch(blur_dir, truth_dir, batch_size):
     while True:
         # Shuffle is in-place
         np.random.shuffle(filenames)
-        i = 0
         for i in range(0, n, batch_size):
             blur_batch, truth_batch = [], []
+            # Python list comp allows going past last iter
             for filename in filenames[i:i+batch_size]:
                 with Image.open(os.path.join(blur_dir, filename), 'r') as blurry:
                     blur_batch.append(np.array(blurry))
